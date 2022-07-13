5addf1141e9341e1c0420eaa378368e5b562c614
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-13 08:18:15 +0800

    Add operate() function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function addition(a, b) {
   return a + b;
-} 
+}
 
 function subtraction(a, b) {
   return a - b;
@@ -11,5 +11,19 @@ function multiplication(a, b) {
 }
 
 function division(a, b) {
-  return (a / b).toFixed(2);
+  if (a % b == 0) return a / b;
+  return parseFloat((a / b).toFixed(2));
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case '+':
+      return addition(a, b);
+    case '-':
+      return subtraction(a, b);
+    case 'x':
+      return multiplication(a, b);
+    case '/':
+      return division(a, b);
+  }
 }
